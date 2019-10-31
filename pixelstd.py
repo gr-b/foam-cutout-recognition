4f7d64564663d979b7a232fd2089eb8aa9b2c76c
thatrobotguy <andrewtheschueler@gmail.com>
2019-10-30 21:57:44 -0400

    I have optimized as much as possible without cuda or c++

--- pixelstd.py
+++ pixelstd.py
@@ -26,33 +26,12 @@ class PixelSDT:
         # Get source image dimensions
         h = image.shape[0]
         w = image.shape[1]
-        print("image shape: ")
-        print(image.shape)
-        print("Image type: ")
-        print(image.dtype)
         # Create mask with same dims but 1 channel
         themask = np.zeros((h,w,1), dtype=image.dtype)
-        print("Got here ")
-        count = 0
-        # stat.stdev(pixel) > self.thresh
         for y in range(0, h):
             for x in range(0, w):
                 # image[y, x] is the pixel location
-                #count = count + 1
-                pixel = image[y, x]
-                pi1 = float(pixel[0])
-                pi2 = float(pixel[1])
-                pi3 = float(pixel[2])
-                #print(count)
-                #print(pixel)
-                output = statistics.stdev((pi1, pi2, pi3))
-                #print("STD: "+str(output))
-                if output > self.thresh:
-                    themask[y, x] = 1
-                else:
-                    themask[y, x] = 0
-                #themask[y, x] = 1 if stat.stdev(pixel) > self.thresh else 0
-                #print(count)
+                themask[y, x] = 1 if statistics.stdev((float(image[y, x][0]), float(image[y, x][1]), float(image[y, x][2]))) > self.thresh else 0
         # Now that the mask is created, we can use the mask
         return cv2.bitwise_and(image,image, mask= themask)
 
@@ -60,7 +39,7 @@ class PixelSDT:
 if __name__=='__main__':
     #img = cv2.imread('../testimages/longneck/close/3.jpg')
     #img = cv2.imread('../testimages/longneck/far/175.jpg')
-    img = cv2.imread('../testimages/longneck/far/140.jpg')
+    img = cv2.imread('../testimages/longneck/far/120.jpg')
     # Try out different float values into the constructor
     thefilter = PixelSDT(9)
     res = thefilter.stdfilter(img)
